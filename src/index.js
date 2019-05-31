1e199d10a542df326824ca875ae500e828af48f1
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-05-31 14:51:39 +0200

    3. Rendering Elements

--- src/index.js
+++ src/index.js
@@ -1,41 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import avatar from './components/images/me.png';
 import * as serviceWorker from './serviceWorker';
 
-function formatName(user) {
-	return `${user.firstName} ${user.lastName}`;
+function tick(user) {
+	const element = (
+		<>
+			<h1>Witaj świecie!</h1>
+			<h2>
+				Aktualny czas: {new Date().toLocaleTimeString()}
+			</h2>
+		</>
+	);
+	ReactDOM.render(element, document.getElementById('root'));
 }
 
-function getGreeting(user) {
-	if (user) {
-		return <h1>Witaj, {formatName(user)}!</h1>;
-	}
-	return <h1>Witaj, nieznajomy</h1>;
-}
-
-const user = {
-	firstName: 'Jakub',
-	lastName: 'Schulz',
-	avatarUrl: { avatar }
-};
-
-const reactElement = React.createElement(
-	'h1',
-	{ className: 'greeting' },
-	'Witaj świecie!'
-);
-
-const element = (
-	<>
-		{reactElement}
-		<img className="avatar" src={avatar} alt="" />
-		{getGreeting(user)}
-		<div tabIndex="0">Tab - test</div>
-	</>
-);
-
-ReactDOM.render(element, document.getElementById('root'));
+setInterval(tick, 1000);
 
 serviceWorker.unregister();
