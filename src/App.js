4a62b4d7d9c01b09113be9bec4c8d065abe044bd
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-05-31 17:03:52 +0200

    5. State and Livecycle

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import Welcome from './components/Scripts/Welcome';
-import Comment, {comment} from './components/Scripts/Comment/Comment'
+import Clock from './components/Scripts/Clock/Clock'
 
 function App() {
-  return (
-    <>
-      <Welcome name="Jakub" />
-      <Welcome name="Paulina" />
-      <Welcome name="Sabina" />
-      <Comment
-        date={comment.date}
-        text={comment.text}
-        author={comment.author}
-      />
-    </>
-  );
+	return (
+		<>
+			<Clock />
+			<Clock />
+			<Clock />
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App'
 import * as serviceWorker from './serviceWorker';
+import App from './App'
 
-
-
-const element = <App/>;
-
-ReactDOM.render(element , document.getElementById('root'));
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
 
 serviceWorker.unregister();
