58cb21c0ffd105ffab1827604a61eeebf1fb9621
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-06-05 18:50:49 +0200

    step 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import SignUpDialog from './components/Scripts/Welcome/WelcomeDialog'
+import FilterableProductTable from './components/Scripts/Search/FilterableProductTable';
 
-// function Contacts() {
-// 	return <div className="Contacts" />;
-// }
-
-// function Chat() {
-// 	return <div className="Chat" />;
-// }
+const PRODUCTS = [
+	{category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+	{category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+	{category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+	{category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+	{category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+	{category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  ];
 
 function App() {
 	return (
 		<>
-			<SignUpDialog />
+			<FilterableProductTable products={PRODUCTS} />
 		</>
 	);
 }
